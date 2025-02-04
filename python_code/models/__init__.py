6d417ef498094c68ae9410554e8eed1c1771b68b
suma1995 <ska@brondby.com>
2025-02-04 09:56:32 +0100

    kl
    knk

--- python_code/models/__init__.py
+++
@@ -1,7 +0,0 @@
-print("init_example is initialized")
-
-from .Agencies import Agencies
-from .Base import Base
-from .CoachMetrics import CoachMetrics
-from .HeadCoach import HeadCoach
-from .Players import Players
