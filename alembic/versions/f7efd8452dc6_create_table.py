3e796d8aa7fff05f63107e3c708a402bf21f35c9
suma1995 <ska@brondby.com>
2025-02-04 12:51:00 +0100

    f
    gf

--- alembic/versions/f7efd8452dc6_create_table.py
+++ alembic/versions/f7efd8452dc6_create_table.py
@@ -1,63 +1,30 @@
-"""create_table
+"""create_tables
 
 Revision ID: f7efd8452dc6
 Revises: 
 Create Date: 2025-02-03 13:22:23.621979
-
 """
-from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'f7efd8452dc6'
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = 'f7efd8452dc6'
+down_revision = None
+branch_labels = None
+depends_on = None
 
-
-def upgrade() -> None:
-
+def upgrade():
+    # Create 'Country' table
     op.create_table(
         'Country',
         sa.Column('Country_id', sa.Integer, primary_key=True),
         sa.Column('Name', sa.String),
     )
-
-    op.create_table(
-        'Competition',
-        sa.Column('Competition_id', sa.Integer, primary_key=True),
-        sa.Column('Competitionname', sa.String),
-        sa.Column('Country_id', sa.Integer, sa.ForeignKey('Country_id')),
-    )
-
-    op.create_table(
-        'Teams',
-        sa.Column('Team_id', sa.String, primary_key=True),
-        sa.Column('Teamname', sa.String),
-        sa.Column('Competition_id', sa.Integer, sa.ForeignKey('Competition.Competition_id')),
-        sa.Column('Country_id', sa.Integer, sa.ForeignKey('Country_id')),
-    )
-
-    op.create_table(
-    'Players',
-    sa.Column('PlayerID', sa.Integer, primary_key=True),
-    sa.Column('Name', sa.String(100), nullable=False),
-    sa.Column('BirthDate', sa.Date),
-    sa.Column('FirstPosition', sa.String(100)),
-    sa.Column('Nationality1', sa.String(100)),
-    sa.Column('Nationality2', sa.String(100), nullable=True),
-    sa.Column('ParentTeam', sa.String(100)),
-    sa.Column('Competition_id', sa.Integer, sa.ForeignKey('Competition.Competition_id')),
-    sa.Column('Team_id', sa.Integer, sa.ForeignKey('teams.Team_id'))
-    )
-
-
-
-def downgrade() -> None:
+    
+    
+def downgrade():
+    
+    
+    # Drop 'Country' table
     op.drop_table('Country')
-    op.drop_table('Competition')
-    op.drop_table('Teams')
-    op.drop_table('Players')
