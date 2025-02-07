c1841bd9b0dc226499528021e2c7c0658c5e8fb0
saffibrondby <sad@brondby.com>
2025-02-07 12:23:05 +0100

    1
    1

--- python_code/yourproject.models/Competition.py
+++ python_code/yourproject.models/Competition.py
@@ -9,7 +9,6 @@ columns_dict = {
     "Competition_id": Column(Integer, primary_key=True),
     "Competitionname": Column(String(100)),
     "divisionLevel" : Column(Integer) , 
-    #"Country_id": Column(Integer, ForeignKey=True),
     }
 
 class Competition(Base):
@@ -18,11 +17,11 @@ class Competition(Base):
     Competition_id = Column(Integer, primary_key=True)
     Competitionname = Column(String(100))
     divisionLevel = Column(Integer) 
-    #Country_id = Column(Integer, ForeignKey("Country.Country_id"))
+    Country_id = Column(Integer, ForeignKey('Country.Country_id'))
     
-    # Relationships
-    #country = relationship("Country", back_populates="competitions")
-   # teams = relationship("Teams", back_populates="competition")
+    #Relationships
+    country = relationship('Country', back_populates='competitions')
+    #teams = relationship("Teams", back_populates="competition")
 
     def __repr__(self):
         return f"Competition(id={self.Competition_id!r}, Competitionname={self.Competitionname!r}, divisionLevel={self.divisionLevel!r}, Country_id={self.Country_id!r})"
