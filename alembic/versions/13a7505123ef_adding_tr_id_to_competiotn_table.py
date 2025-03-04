e161d6f0fa465a9408c3966bfa738d5feed6413b
suma1995 <ska@brondby.com>
2025-03-04 13:09:42 +0100

    adding player metrics

--- alembic/versions/13a7505123ef_adding_tr_id_to_competiotn_table.py
+++ alembic/versions/13a7505123ef_adding_tr_id_to_competiotn_table.py
@@ -17,14 +17,14 @@ down_revision: Union[str, None] = '0ae36a2ae109'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 def upgrade():
-    # Step 1: Add the column as nullable
-    op.add_column("Competition", sa.Column("TR_com_ID", sa.Integer(), nullable=True))
+   
+    op.add_column("PlayerMetrics", sa.Column("GBEScore", sa.Integer, nullable=True))
+    op.add_column("PlayerMetrics", sa.Column("BaseValue", sa.Numeric, nullable=True))
+    op.add_column("PlayerMetrics", sa.Column("EstimatedSalary", sa.String, nullable=True))
 
-    # Step 2: Set a default value (if needed) for existing rows
-    op.execute("UPDATE Competition SET TR_com_ID = 0")  # or another meaningful default
-
-    # Step 3: Make the column non-nullable after setting a default
-    op.alter_column("Competition", "TR_com_ID", nullable=False)
+    
 
 def downgrade():
-    op.drop_column("Competition", "TR_com_ID")
+    op.drop_column("PlayerMetrics", "GBEScore")
+    op.drop_column("PlayerMetrics", "BaseValue")
+    op.drop_column("PlayerMetrics", "EstimatedSalary")
